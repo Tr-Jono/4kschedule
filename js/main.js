a9d2ef80ae12bded9bea8a4f0de6bef5649b7f10
Tr-Jono <omgthisissouseless@gmail.com>
2020-04-02 01:28:49 +0800

    add epic flashing colors

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function changeColor() {
+	imgs.forEach(img => img.style.borderColor = colors[index]);
+	index = (index + 1) % 7;
+	setTimeout(changeColor, 150);
+}
+
+let index = 0;
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+const imgs = [...document.getElementsByClassName("beefball")];
+if (imgs.length) {
+	changeColor();
+}
