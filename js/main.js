23eb5aba3379e614103bf3f11076e1b60795f35b
Tr-Jono <omgthisissouseless@gmail.com>
2020-04-07 12:18:34 +0800

    add favico and rewrite JS

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
-function changeColor() {
-	imgs.forEach(img => img.style.borderColor = colors[index]);
-	index = (index + 1) % 7;
-	setTimeout(changeColor, 150);
-}
-
 let index = 0;
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 const imgs = document.querySelectorAll("img");
 if (imgs.length)
-	changeColor();
+	setInterval(() => {
+		imgs.forEach(img => img.style.borderColor = colors[index]);
+		index = (index + 1) % 7;
+	}, 150);
